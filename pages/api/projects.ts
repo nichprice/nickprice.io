e5dd664733628193c4f19f954e755c7654df0f95
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-02-12 16:07:59 -0500

    prisma and planetscale working

---
+++ pages/api/projects.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+// POST /api/user
+// Required fields in body: name, email
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const result = await prisma.project.findMany();
+  return res.status(201).json(result);
+}
