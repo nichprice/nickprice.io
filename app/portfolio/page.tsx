373f32603983016a51eb9aeb10c049cbca0c45e8
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-02-13 20:03:26 -0500

    installed swr

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,3 +1,17 @@
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function portfolio() {
-  return <h1 className="text-3xl font-bold underline">My Portfolio</h1>;
+  const { data, error } = useSWR("/api/projects", fetcher);
+
+  if (error) return <div>Failed to load.</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">My Portfolio</h1>
+      <p>{data.title}</p>
+    </>
+  );
 }
