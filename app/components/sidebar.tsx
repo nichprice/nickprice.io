7f5fb2fd5af88e4969c013db251085e2e4f7477d
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-02-16 10:58:48 -0500

    clean operational nav bar w random console errors

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -17,24 +17,22 @@ const navItems = {
 
 export default function Navbar() {
   return (
-    <main className="bg-white px-12">
-      <section className="min-h-screen">
-        <nav className="py-10 mb-12 flex justify-between">
-          <Link key="/" href="/">
-            home
-          </Link>
-          <ul className="flex items-center">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              const isActive = path;
-              return (
-                <Link className="px-8" key={path} href={path}>
-                  {name}
-                </Link>
-              );
-            })}
-          </ul>
-        </nav>
-      </section>
-    </main>
+    <nav className="py-10 mb-12 flex justify-between">
+      <Link className="px-8" key="/" href="/">
+        home
+      </Link>
+      <ul className="flex items-center">
+        {Object.entries(navItems).map(([path, { name }]) => {
+          const isActive = path;
+          return (
+            <li>
+              <Link className="px-8" key={path} href={path}>
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-      </body>
-    </html>
+    <div>
+      <head>
+        <title>Nicholas Price's Portfolio</title>
+      </head>
+
+      <main className="bg-white px-12">
+        <section className="min-h-screen">
+          <Navbar />
+          <div>{children}</div>
+        </section>
+      </main>
+    </div>
   );
 }
