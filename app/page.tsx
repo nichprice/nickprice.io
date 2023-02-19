c6b0fdbf954a6ec85f9a887fc45cde1efc1ec1ea
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-02-19 11:04:28 -0500

    image sizing fixed!!

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 import { AiFillLinkedin, AiFillYoutube, AiFillGithub } from "react-icons/ai";
 import Image from "next/image";
-import dinner_me from "public/photos/dinner-me.jpeg";
+import dinner_pic from "public/photos/dinner-pic.jpeg";
 
 export default function Home() {
   return (
     <section>
-      <div className="py-14">
+      <div className="pt-8">
         <h2 className="text-5xl font-medium flex justify-center gap-8 py-3 text-sky-600 ">
           Nick Price
         </h2>
-        <h3 className="text-2xl font-medium flex justify-center gap-8 py-1 text-gray-600 opacity-80">
+        <h3 className="text-2xl font-medium flex justify-center gap-8 py-3 text-gray-600 opacity-80">
           Developer
         </h3>
       </div>
-      <Image src={dinner_me} alt="headshot" className="mx-auto rounded-full" />
-      <div className="text-5xl flex justify-center gap-8 py-3 opacity-90">
+      <Image
+        src={dinner_pic}
+        alt="headshot"
+        className="mx-auto rounded-full w-80 h-80"
+      />
+      <div className="text-5xl flex justify-center gap-8 py-5 opacity-90">
         <a
           href="https://www.linkedin.com/in/price-nicholas/"
           className="text-blue-800"
