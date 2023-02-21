6aa033611ee202fdf9620608e249196e1ce251e0
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-02-21 12:58:12 -0500

    deleted redundant project holder, moved code to portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,12 +1,34 @@
 "use client";
-import ProjectsHolder from "../components/projectsholder";
+import React, { useEffect, useState } from "react";
+import ProjectCard from "../components/projectcard";
 
 export default function PortfolioPage() {
+  const [projects, setData] = useState<any[]>([]);
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/api/projects")
+      .then((res) => res.json())
+      .then((projects) => {
+        setData(projects);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log("hi");
+  console.log(projects);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (!projects) return <p>No profile data</p>;
+
   return (
     <div>
       <h1 className="text-3xl font-bold">Everything so far</h1>
       <div>
-        <ProjectsHolder />
+        {projects.map((project: { id: React.Key | null | undefined }) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
       </div>
     </div>
   );
