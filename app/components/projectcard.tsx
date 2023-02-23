e755d350521aa4793669d9b726ea585dcff59560
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-02-23 11:44:08 -0500

    working rough site, need to merge to main

--- app/components/projectcard.tsx
+++ app/components/projectcard.tsx
@@ -3,13 +3,15 @@ import YouTube from "react-youtube";
 export default function ProjectCard({ project }: { project: any }) {
   return (
     <div className="rounded-lg shadow-md h-[30] w-[40] py-5 px-5 my-5 flex justify-center">
-      <div className="mx-4">
+      <div className="mx-4 bg-orange-200">
         <h2 className="text-3xl font-semibold pt-8 pb-6">{project.title}</h2>
         <h4>{project.tech}</h4>
         <p>{project.about}</p>
       </div>
-      <div>
-        <YouTube videoId={project.youtube} />
+      <div className="">
+        <div className="mx-10 my-10">
+          <YouTube videoId={project.youtube} />
+        </div>
       </div>
     </div>
   );
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,41 +1,43 @@
 "use client";
 
+import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
 const navItems = {
-  "/": { name: "home" },
   "/about": { name: "about" },
   "/portfolio": { name: "portfolio" },
   "/blog": { name: "blog" },
 };
 
 export default function Navbar() {
+  let pathname = usePathname();
+
   return (
-    <nav className="py-5 px-6 mb-12 flex justify-between bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-orange-400 to-sky-400">
-      <Link
-        href="/"
-        className="text-white text-xl hover:opacity-70 border-b-2: isActive border-b-white:isActive "
-      >
-        home
-      </Link>
-      <ul className="flex items-center text-white text-xl">
-        <li>
-          <Link href="/about" className="px-8 hover:opacity-70">
-            about
-          </Link>
-        </li>
-        <li>
-          <Link href="/portfolio" className="px-8 hover:opacity-70">
-            portfolio
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog" className="px-8 hover:opacity-70">
-            blog
-          </Link>
-        </li>
-      </ul>
+    <nav className="py-5 mb-12  bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-orange-400 to-sky-400">
+      <div className="flex justify-end px-10">
+        {Object.entries(navItems).map(([path, { name }]) => {
+          const isActive = path === pathname;
+          return (
+            <div
+              className={clsx({
+                "rounded-xl": isActive,
+                "opacity-30": isActive,
+                "bg-neutral-100": isActive,
+              })}
+            >
+              <Link
+                href={path}
+                className={clsx("text-white px-4 hover:opacity-70", {
+                  "opacity-100": isActive,
+                })}
+              >
+                {name}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </nav>
   );
 }
