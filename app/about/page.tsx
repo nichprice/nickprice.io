0cf0a884f11bb9159c27c6b2947d0d9fadf5ea7e
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-03-01 11:17:52 -0500

    progress on abt page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import ski_lift from "public/photos/ski-lift.jpeg";
 import pasta from "public/photos/pasta.jpg";
 import trivia from "public/photos/trivia_team.jpeg";
 import f1 from "public/photos/f1.jpeg";
-import Abtmeblocks from "app/components/abtmeblocks.tsx";
+import Image from "next/image";
 
 const abouts = {
   baking: { desc: "baking", image: bread },
@@ -24,9 +24,52 @@ export default function About() {
         </p>
       </div>
       <div>
-        {abouts.map((about) => (
-          <Abtmeblocks about={about} />
-        ))}
+        <h3 className="text-center">Tech I've worked with</h3>
+        <ul className="flex flex-col gap-10 py-10 flex-col flex-wrap">
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>React</li>
+          <li>Python</li>
+          <li>Django</li>
+          <li>Node.js</li>
+          <li>Prisma</li>
+          <li>Ruby</li>
+          <li>Ruby on Rails</li>
+          <li>PlanetScale</li>
+          <li>SQL</li>
+          <li>MySQL</li>
+          <li>SQLite</li>
+          <li>CSS</li>
+          <li>Tailwind</li>
+          <li>HTML</li>
+        </ul>
+      </div>
+      <div>
+        <h3>For fun...</h3>
+        <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+          <div className="basis-1/3 flex-1 ">
+            <Image src={f1} alt="F1 car at the Austin GP" />
+            <div>
+              <p>f1</p>
+            </div>
+          </div>
+          <div className="basis-1/3 flex-1 ">
+            <Image src={f1} alt="F1 car at the Austin GP" />
+            <div>
+              <p>f1</p>
+            </div>
+          </div>
+          <div className="basis-1/3 flex-1 ">
+            <Image
+              src={f1}
+              alt="F1 car at the Austin GP"
+              className="object-cover"
+            />
+            <div>
+              <p className="flex mx-auto">f1</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
