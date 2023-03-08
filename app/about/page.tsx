60dd4a2dd082ef47abf4999d931dab8c44eef175
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-03-08 15:29:46 -0500

    about looking a lot better

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -25,53 +25,21 @@ export default function About() {
       <h1 className="text-5xl font-medium flex justify-center text-sky-600 pb-10">
         About me
       </h1>
-      <div className="md:flex mx-auto">
+      <div className="mx-auto">
         <div className="md:max-w-lg mx-auto">
           <h3 className="text-3xl font-semibold pb-6">
             Thanks for stopping by!
           </h3>
 
-          <p className="font-normal leading-relaxed pb-4">
-            I {"(attempt to)"} write code! I've been learning about software
-            engineering and web developement for about a year now, and have been
-            having a blast doing so. I split most of my time between praticing
-            the fundamentals and learning new technologies. I believe the best
-            approach to learning involes focusing on the basics and core
-            concepts that explain of how/why modern tech is the way it is. With
-            that understanding new tech becomes more useful and more powerful.
-          </p>
-          <p className="font-normal pb-4">
-            I've always been a bit of a nerd at heart. I enjoy numbers and
-            science {"(favorite highschool classes: Accounting and AP Chem)"}{" "}
-            and I also really enjoy understanding how things work and the
-            stories that explain why that is{" "}
-            {
-              "(favorite college courses: Developement of Europe, Anatomy, and Physics I & II)"
-            }
-            . I love teamwork and collaboration {"(Country > Track and Field)"},
-            and I really like to flex my creative muscle from time to time{" "}
-            {
-              "(my childhood involved a lot of playing with legos and building remote control cars)"
-            }
-            .
-          </p>
-          <p className="font-normal pb-4">
-            I spend a lot of time nerding out on exercise science. My two
-            favorite ways to excercise are cycling and lifting. I'm a big sports
-            fan, namely F1 and football. Although I'm happy as long as there's
-            good racing in F1, I really admire Toto Wolff and what he's been
-            able to accomplish in the sport so I find myself a defacto Mercedes
-            fan. When it comes to football, I'm probably the biggest Jets fan
-            you'll ever meet.
-          </p>
-          <p className="font-normal pb-4">
-            I really enjoy cooking and coffee. Learning from Thomas Keller,
-            Marco Pierre White, and of course, Babish, has completely changed
-            how I approach both cooking and eatting. To me coffee is more than
-            just how I start my day, it's a hobby. There's something special
-            about grinding fresh beans and watching water slowly boil through
-            the spout of a Moka pot.
-          </p>
+          <ul>
+            <li>I write code! Mostly JS and Python.</li>
+            <li>I’m passionate about science and problem solving.</li>
+            <li>I love lifting and endurance training.</li>
+            <li>Toto Wolff is my hero.</li>
+            <li>Thomas Keller changed the way I cook.</li>
+            <li>I’m probably the biggest Jets fan you’ll ever meet.</li>
+            <li>I’m a diehard Mokapot enthusiest.</li>
+          </ul>
         </div>
         <div className="mx-auto">
           <p className="text-3xl font-semibold pt-8 pb-6">Let's chat about:</p>
