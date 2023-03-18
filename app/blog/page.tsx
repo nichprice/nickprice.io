5ac64000cf807fb6c4c2cc6869c05c0a254a3b89
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-03-18 16:30:56 -0400

    posts directory removed and blog page populated w temp code

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,24 +1,7 @@
-import PostCard from "../components/postcard";
-import { allPosts } from "contentlayer/generated";
-import { compareDesc } from "date-fns";
-
-export async function getStaticProps() {
-  const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date));
-  });
-  return { props: { posts } };
-}
-
-export default function Home({ posts }) {
+export default function Blog() {
   return (
-    <div className="mx-auto max-w-2xl py-16 text-center">
-      <title>Contentlayer Blog Example</title>
-
-      <h1 className="mb-8 text-3xl font-bold">Contentlayer Blog Example</h1>
-
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
+    <div>
+      <h1>This is where the blog will go.</h1>
     </div>
   );
 }
