495a4a6a560e08b899a704994a689e26f1a8b21f
Nichprice <97806706+Nichprice@users.noreply.github.com>
2023-03-18 20:19:32 -0400

    contentlayer working!

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,25 @@
-import { compareDesc } from 'date-fns'
-import { allPosts } from 'contentlayer/generated'
-import PostCard from '../components/postcard'
-
-export async function getStaticProps() {
-  const posts = allPosts.sort((a, b) => {
-    return compareDesc(new Date(a.date), new Date(b.date))
-  })
-  return { props: { posts } }
+import Link from "next/link";
+import { compareDesc, format, parseISO } from "date-fns";
+import { allPosts } from "contentlayer/generated";
+import PostCard from "../components/postcard";
 
 export default function Blog() {
   return (
-    <div>
+    <div className="mx-auto max-w-2xl py-16 text-center">
+      <title>Contentlayer Blog Example</title>
+
       <h1 className="mb-8 text-3xl font-bold">Contentlayer Blog Example</h1>
 
-      {posts.map((post, idx) => (
-        <PostCard key={idx} {...post} />
-      ))}
+      {allPosts
+        .sort((a, b) => {
+          if (new Date(a.date) > new Date(b.date)) {
+            return -1;
+          }
+          return 1;
+        })
+        .map((post, idx) => (
+          <PostCard key={idx} post={post} />
+        ))}
     </div>
   );
 }
--- app/components/postcard.tsx
+++ app/components/postcard.tsx
@@ -1,16 +1,14 @@
 import Link from "next/link";
 import { format, parseISO } from "date-fns";
 
-export default function PostCard({ post }) {
+export default function PostCard({ post }: { post: any }) {
   return (
     <div className="mb-6">
       <time dateTime={post.date} className="block text-sm text-slate-600">
         {format(parseISO(post.date), "LLLL d, yyyy")}
       </time>
       <h2 className="text-lg">
-        <Link href={post.url}>
-          <a className="text-blue-700 hover:text-blue-900">{post.title}</a>
-        </Link>
+        <Link href={post.url}>{post.title}</Link>
       </h2>
     </div>
   );
